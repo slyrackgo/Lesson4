0a51835fce377bcd344456104ac16d153c2480e0
Atai <aibragimov2003@bk.ru>
2026-02-11 09:38:31 +0600

    классы обьеденены

--- Main.java
+++ Main.java
@@ -1,19 +1,30 @@
+import javax.print.Doc;
 import java.util.Objects;
 
 public class Main {
      public static void main(String[] args) {
+         Hospital hospital = new Hospital("Городская Больница", "4 мкр");
+         Doctor doctor = new Doctor("Askar", "Стоматолог", "Городская Больница", "4 мкр");
 
+         System.out.println(hospital.getAddress());
+         System.out.println(hospital.getHospitalName());
+
+         hospital.work();
      }
  }
 
 
-class Hospital{
+class Hospital implements Workable{
     private String hospitalName;
     private String address;
 
     public Hospital(String hospitalName, String address){
         this.address = address;
         this.hospitalName = hospitalName;
+    }
+
+    public void work(){
+        System.out.println("Я работаю стоматологом в больнице: " + getHospitalName());
     }
 
     public String getHospitalName(){
@@ -25,4 +36,18 @@ class Hospital{
     }
 }
 
+class Doctor extends Hospital{
+    private String name;
+    private String specialisation;
+
+    public Doctor(String name, String specialisation, String hospitalName, String address){
+        this.name = name;
+        this.specialisation = specialisation;
+        super(hospitalName, address);
+    }
+}
+
+interface Workable {
+    void work();
+}
 
